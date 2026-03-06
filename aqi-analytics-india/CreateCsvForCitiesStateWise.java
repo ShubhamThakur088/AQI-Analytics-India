cb17c4dd8e3fd952980ecac0f4e0999b0ec84e65
ShubhamKThakur <88872926+ShubhamThakur088@users.noreply.github.com>
2026-03-06 11:12:22 +0530

    built logic to create state-wise csv files

--- aqi-analytics-india/CreateCsvForCitiesStateWise.java
+++ aqi-analytics-india/CreateCsvForCitiesStateWise.java
@@ -1,9 +1,11 @@
 package collectionshandson;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Iterator;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
@@ -15,6 +17,29 @@ import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 public class CreateCsvForCitiesStateWise {
+	
+	
+	public static void createCSV(Map<Integer, String> cityAqiDetailsMaps, List<String> stateName) throws IOException {
+		String filePath = new String("D:\\CSV-Files\\aqi-analytics-india\\");
+		
+		String fileName = filePath+stateName.get(0).toLowerCase()+"_state_cities_aqi_data.csv";		
+		File file = new File(fileName);
+		file.createNewFile();
+		
+		try(BufferedWriter fileWriter = new BufferedWriter(new FileWriter(file))){
+			fileWriter.write("date,city_name,air_quality_status,aqi_value");
+			fileWriter.newLine();
+			
+			for(Map.Entry<Integer, String> map : cityAqiDetailsMaps.entrySet()) {
+				String value = map.getValue();
+				
+				fileWriter.write(value);
+				fileWriter.newLine();
+			}			
+		}
+	}
+	
+	
 
 	public static void main(String args[]) throws IOException {
 		String filePath = new String("D:\\CSV-Files\\aqi-analytics-india\\aqi_india_excel_format.xlsx");
@@ -25,7 +50,7 @@ public class CreateCsvForCitiesStateWise {
 		int i = 0;
 		int j = 0;
 		
-		List<String> statesList = new ArrayList<>(Arrays.asList("Karnataka"));//Andhra Pradesh, Arunachal Pradesh, Assam, Bihar, Chandigarh, Chhattisgarh, Delhi, Goa, Gujarat, Haryana, Himachal Pradesh, Jammu and Kashmir, Jharkhand, Karnataka, Kerala, Ladakh, Madhya Pradesh, Maharashtra, Manipur, Meghalaya, Mizoram, Nagaland, Odisha, Puducherry, Punjab, Rajasthan, Sikkim, Tamil Nadu, Telangana, Tripura, Uttar Pradesh, Uttarakhand, West Bengal
+		List<String> statesList = new ArrayList<>(Arrays.asList("Andhra Pradesh"));//Andhra Pradesh, Arunachal Pradesh, Assam, Bihar, Chandigarh, Chhattisgarh, Delhi, Goa, Gujarat, Haryana, Himachal Pradesh, Jammu and Kashmir, Jharkhand, Karnataka, Kerala, Ladakh, Madhya Pradesh, Maharashtra, Manipur, Meghalaya, Mizoram, Nagaland, Odisha, Puducherry, Punjab, Rajasthan, Sikkim, Tamil Nadu, Telangana, Tripura, Uttar Pradesh, Uttarakhand, West Bengal
 		Map<Integer, String> cityAqiDetailsMap = new LinkedHashMap<>();
 		
 		
@@ -41,7 +66,7 @@ public class CreateCsvForCitiesStateWise {
 
 					if (typeOfCell == CellType.STRING) {
 						if (cells.getStringCellValue().toString().trim().equalsIgnoreCase(statesList.get(i))) {
-							String areaDetails = cells.getRow().getCell(2)+" "+ " " + cells.getRow().getCell(6) + " "+ cells.getRow().getCell(5);
+							String areaDetails =cells.getRow().getCell(0)+ "," +cells.getRow().getCell(2)+ "," + cells.getRow().getCell(6) +","+ cells.getRow().getCell(5);
 							cityAqiDetailsMap.put(j, areaDetails);							
 						}
 					}
@@ -50,8 +75,11 @@ public class CreateCsvForCitiesStateWise {
 			}
 			i++;
 		}
-		System.out.println(cityAqiDetailsMap.size());
-		System.out.println(cityAqiDetailsMap);
+//		System.out.println(cityAqiDetailsMap.size());
+//		System.out.println(cityAqiDetailsMap);
 		workBook.close();
+		
+		createCSV(cityAqiDetailsMap, statesList);
 	}
+	
 }
